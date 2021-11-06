40322ab04d1f4a6ff7ab586c92d9a9c229294979
rachit <rachau76@gmail.com>
2021-11-06 13:45:34 +0530

    added theme

---
+++ lib/theme.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class OurTheme {
+  Color primaryColors = const Color(0xFF202124);
+  Color secondaryColor = const Color(0xFFFADFCF);
+  Color tertiaryColor = const Color(0xFFFFFFFF);
+  String font = "Quicksand";
+}
